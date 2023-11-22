3a14bc5ef508e367fc12ac64fdcd58cde200658c
Trisha Angel P. Millena <trisha.millena@obf.ateneo.edu>
2023-11-22 17:53:59 +0800

    Edited models.py of Items App

--- Items/models.py
+++ Items/models.py
@@ -21,14 +21,42 @@ class Item(models.Model):
     item_type = models.CharField(max_length = 300, choices = ITEM_TYPE_CHOICES)
     item_cost = models.DecimalField(max_digits = None, decimal_places = 3) #not sure if none
     item_SRP = models.DecimalField(max_digits = None, decimal_places = 3) #not sure if none
-    item_total_cost = models.DecimalField(max_digits = 10, decimal_places = 3)
-    item_total_sales = models.DecimalField(max_digits =10, decimal_places = 3)
+    item_total_cost = models.DecimalField(max_digits = 10, decimal_places = 2, editable = False)
+
+    def save(self, *args, **kwargs):
+        self.total_cost = self.item_total_cost * self.item_qty
+        super().save(*args, **kwargs)
+
+class SoldItem(models.Model):
+    item = models.OneToOneField(Item, on_delete = models.CASCADE)
+    sold_quantiy = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        self.total_sales = self.sold_quantity * self.item_total_cost
+    
+    def __str__(self):
+        return f"SoldItem: {self.item.item_brand} {self.item.item_model}"
 
 class DeliveredItem(models.Model):
     item = models.OneToOneField(Item, on_delete = models.CASCADE)
+    delivered_quantity = models.IntegerField()
 
     def __str__(self):
-        return f"DeliveredItem: {self.item.item_brand} {self.staff.item_model}"
+        return f"DeliveredItem: {self.item.item_brand} {self.item.item_model}"
+
+class IssuedItem(models.Model):
+    item = models.OneToOneField(Item, on_delete = models.CASCADE)
+    issued_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"IssuedItem: {self.item.item_brand} {self.item.item_model}"
+
+class ReturnedItem(models.Model):
+    item = models.OneToOneField(Item, on_delete = models.CASCADE)
+    quantity_returned = models.IntegerField()
+
+    def __str__(self):
+        return f"ReturnedItem: {self.item.item_brand} {self.staff.item_model}"
 
 class BatchInventory(models.Model):
     batch_inventory = models.ManyToManyField(DeliveredItem)
