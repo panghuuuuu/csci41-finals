321e072f5d3842835fd00068ed40f539fc04df9f
Nisha <julia.espera@obf.ateneo.edu>
2023-11-27 12:53:21 +0800

    added filter functions

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from staff.models import Receiver
+from supplier.models import Supplier
+
+class DeliveryReceipt(models.Model):
+    delivery_number = models.AutoField(primary_key = True)
+    date = models.CharField(max_length = 100)
+    time = models.CharField(max_length = 15)
+    receiver = models.ForeignKey(Receiver, on_delete = models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete = models.CASCADE)
+    
--- delivery/views.py
+++ delivery/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from items.models import DeliveryReceipt
+
+def filter_view(request):
+    qs = DeliveryReceipt.objects.all()
+    byItem_query = request.GET.get('search_by_item')
+    byItemType_query = request.GET.get('search_by_item_type')
+    byDate_query = request.GET.get('search_by_date')
+    
+    if byItem_query != '':
+        qs = qs.filter(title__icontains = byItem_query)
+
+    elif byItemType_query != '':
+        qs = qs.filter(title__icontains = byItemType_query)
+        
+    elif byDate_query != '':
+        qs = qs.filter(title__icontains = byDate_query)    
+    
+    context = {
+        'queryset': qs
+    }
+    return render(request, 'delivery/filter.html', context)
+
+
