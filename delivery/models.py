666f21ced1e09ad8b66611506981220dbdfd8b9b
Nisha <julia.espera@obf.ateneo.edu>
2023-11-27 14:17:43 +0800

    edited models.py

--- delivery/models.py
+++ delivery/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from staff.models import Receiver
+from staff.models import Receiver, Order
 from supplier.models import Supplier
 
 class DeliveryReceipt(models.Model):
@@ -9,4 +9,8 @@ class DeliveryReceipt(models.Model):
     time = models.CharField(max_length = 15)
     receiver = models.ForeignKey(Receiver, on_delete = models.CASCADE)
     supplier = models.ForeignKey(Supplier, on_delete = models.CASCADE)
-    
+    items = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    #get supplier of the order
+    def get_supplier(self):
+        return self.items.supplier if self.items else None
