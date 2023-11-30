61a1db1c58c280656a71f18bdb43c9e90818c17a
Nisha <julia.espera@obf.ateneo.edu>
2023-12-01 04:08:37 +0800

    edited delivery models

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from items.models import Item
+from .models import *
+
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ('receiver', 'supplier')
 
 class ItemInline(admin.TabularInline):
     model = Item
@@ -8,3 +12,5 @@ class deliveryAdmin(admin.ModelAdmin):
     inlines = [
         ItemInline,
     ]
+
+admin.site.register(Delivery, DeliveryAdmin)
--- delivery/models.py
+++ delivery/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-from staff.models import Receiver, Order
+from items.models import OrderedItem
+from staff.models import Receiver
 from supplier.models import Supplier
 
-class DeliveryReceipt(models.Model):
+class Delivery(models.Model):
     delivery_number = models.AutoField(primary_key = True)
     date = models.CharField(max_length = 100)
     time = models.CharField(max_length = 15)
     receiver = models.ForeignKey(Receiver, on_delete = models.CASCADE)
     supplier = models.ForeignKey(Supplier, on_delete = models.CASCADE)
-    items = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.OneToOneField(OrderedItem, on_delete = models.CASCADE)
+    delivered_quantity = models.IntegerField(default=0)
 
     #get supplier of the order
-    def get_supplier(self):
-        return self.items.supplier if self.items else None
+    # def get_supplier(self):
+    #     return self.items.supplier if self.items else None
