ee27f1b28590337fb7e77009994fa8f4b9b65751
Brescia Amandy <amandybrescia@gmail.com>
2023-12-04 16:48:49 +0800

    Updated frontend for login page

--- agent/urls.py
+++ agent/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('sales', get_agents, name='get_agents'),
     path('sales/sold_items', input_sales, name='input_sales'),
     path('sales/submit_sales', submit_sales, name='submit_sales'),
-
-
 ]
